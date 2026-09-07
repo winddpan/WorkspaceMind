e1fc2c3677877a666914f847daf28e5791775643
winddpan <winddpan@126.com>
2026-09-07 10:06:25 +0800

    Update Tangrid to 1.6.3

--- Casks/tangrid-app.rb
+++ Casks/tangrid-app.rb
@@ -1,8 +1,8 @@
 cask "tangrid-app" do
-  version "1.6.2"
-  sha256 "38dac5d8eed4818924c9ee8a0bee1f658d4328114812793d639b8cb51ee382d7"
+  version "1.6.3"
+  sha256 "d7804e99799786beebf849159beade495986296aa14938f544960e29901d0ffe"
 
-  url "https://github.com/winddpan/Tangrid/releases/download/1.6.2/tangrid-1.6.2.zip"
+  url "https://github.com/winddpan/Tangrid/releases/download/1.6.3/tangrid-1.6.3.zip"
   name "Tangrid"
   desc "Window manager with snapping, tiling, Window Switcher, Dock previews, Workspace"
   homepage "https://github.com/winddpan/Tangrid"
